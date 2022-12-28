15dac80e3efb6d69f6988344b0d06f1004a1f1da
Rohit <rohit2k02@gmail.com>
2022-12-28 17:04:48 +0530

    Month-wise Increment

--- CSVModifier.py
+++ CSVModifier.py
@@ -22,3 +22,45 @@ df.to_csv('PopulationData.csv',index=False)
 df=pd.read_csv('PopulationData.csv')
 df.Country_Name.replace('Sint Maarten (Dutch part)', 'Sint Maarten', inplace=True)
 df.to_csv('PopulationData.csv',index=False)
+
+#To Sort by CountryCode and Year
+df=pd.read_csv('Population.csv')
+df.sort_values(["Country_Code", "Year"],axis=0, ascending=True,inplace=True,na_position='first')
+df.to_csv('Pop.csv',index=True)
+
+#Duplicate Dataframe 12 Times
+df=pd.read_csv('Population.csv')
+dfcopies = [df] * 12
+result = pd.concat(dfcopies)
+result.to_csv('Population.csv',index=False)
+
+#Assign Month Values
+df=pd.read_csv('Population.csv')
+df['Month'] = pd.np.nan
+for index, row in df.iterrows():
+    df.loc[index, 'Month'] = index % 12 + 1
+df.to_csv('Population.csv', index=False)
+
+#Re-assigning matching Population values for each month of each year for each country
+df=pd.read_csv('Population.csv')
+for cc in range(0, 186):
+    if cc == 47:
+        continue
+    if cc == 59:
+        continue
+    if cc == 87:
+        continue
+    for y in range(1960, 2018):
+        pop0 = df[(df['Country_Code'] == cc) & (df['Year'] == y) & (df['Month'] == 1)]
+        yplus = y + 1
+        if y == 2017:
+            yplus = y
+        pop1 = df[(df['Country_Code'] == cc) & (df['Year'] == yplus) & (df['Month'] == 1)]
+        print(pop1, pop0)
+        monthchange = int((int(pop1['Population']) - int(pop0['Population'])) / 12)
+        print(monthchange)
+        for m in range(1, 13):
+            newpop = df[(df['Country_Code'] == cc) & (df['Year'] == y) & (df['Month'] == m)]
+            df.loc[(df['Country_Code'] == cc) & (df['Year'] == y) & (df['Month'] == m), 'Population'] = \
+                int(newpop['Population']) + (monthchange*m)
+df.to_csv('Population.csv',index=False)
